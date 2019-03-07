ce63f8596658a12f820062e04fbe953c74e597ad
Jason Moore <jason.moore1369@gmail.com>
2019-03-06 22:46:45 -0600

    tried to start

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var pokemonNames = [bulbasaur, ivysaur, venusaur, squirtal, wartortal, blastoise, charmander, charmelion, charizard];
+
+var userText = document.getElementById()
+document.onkeyup = function(event) {
+    userText.textContent = event.key
+};
+
+var wins = 0;
+
+for (var wins = 0; )
