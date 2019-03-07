9b4e469023f10a89d17ec83b61ebff91fa8e8585
Jason Moore <jason.moore1369@gmail.com>
2019-03-07 17:59:55 -0600

    started jva

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,23 @@
-var pokemonNames = [bulbasaur, ivysaur, venusaur, squirtal, wartortal, blastoise, charmander, charmelion, charizard];
+var pokemonNames = ["bulbasaur", "ivysaur", "venusaur", "squirtal", "wartortal", "blastoise", "charmander", "charmelion", "charizard"];
+ 
 
-var userText = document.getElementById()
+var pickPoke = function() {
+    var randomPoke = pokemonNames[Math.floor(Math.random()*pokemonNames.length)];
+    return randomPoke;
+    
+  
+};
+console.log(pickPoke())
+
+
+
+
+var userText = document.getElementById("guess")
 document.onkeyup = function(event) {
+    console.log(event.key)
     userText.textContent = event.key
 };
 
 var wins = 0;
 
-for (var wins = 0; )
+// for (var wins = 0; )
