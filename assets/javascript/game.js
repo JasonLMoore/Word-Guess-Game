733ecc90982c3594422f284de01812a99648a59f
Jason Moore <jason.moore1369@gmail.com>
2019-03-10 21:12:24 -0500

    finished what I could

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,56 @@
 var pokemonNames = ["bulbasaur", "ivysaur", "venusaur", "squirtal", "wartortal", "blastoise", "charmander", "charmelion", "charizard"];
+var guessRemain = 5;
 var wins = 0;
-var userText;
-var guessRemain;
 var lettersGuessed = []
 
  
-
+// pokemon // 
 var pickPoke = function() {
-    var randomPoke = pokemonNames[Math.floor(Math.random()*pokemonNames.length)]; 
-    return randomPoke;
+    var randomPoke = pokemonNames[Math.floor(Math.random()*pokemonNames.length)];
+    
+    console.log(randomPoke) 
+    
+    var hiddenPoke = randomPoke.replace(/[a-z]/gi, '?');
+    return hiddenPoke;
 };
-console.log(pickPoke())
+
+
+var guessPoke = function() {
+    document.onkeyup = function(event) {
+        var playerInput = event.key;
+        // l < length of hiddenPoke; //
+        for (l = 0; l < ; l++) {
+            if (playerInput = l) {
+                hiddenPoke.replace(l, playerInput);
+            }
+        }
+    };
+};
 document.getElementById("pokemon").innerHTML = pickPoke();
+        
+        
+        
 
 
 
 
 
 
+// guess remain //
+var guessCount = function() {
+    // if letter guessed != letters listed //
+        // guessRemain - 1 //
+    // when guessRemain === 0 //
+        // alert (sorry. you lose.) //
+        // run pickPoke() //
 
+};
+console.log(guessRemain)
+
+
+
+
+// letters guessed //
 document.onkeyup = function(event) {
     var playerGuess = event.key;
 
@@ -43,9 +75,17 @@ document.onkeyup = function(event) {
     };
     checkLetters()
     document.getElementById("letters-guessed").innerHTML = lettersGuessed;
-    
 };
 
+// wins //
+    // if no index pickPoke() = '?' //
+        // wins = wins + 1 //
+    // when wins = 10 //
+        // alert(Congrats! You Win!) //
+        // reset game //
+
+// everything called //
+guessPoke()
     
     
     
